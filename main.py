e398c53f762b7e9e4a5e6e2e1b4ab5d003bf1e8f
Park-jiyoon <bbpgy1995@gmail.com>
2022-03-21 22:38:18 +0900

    원하는 태그 선택하기 3

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ rating_page = response.text
 soup = BeautifulSoup(rating_page, 'html.parser')
 
 tr_tag = soup.select('tr')[1]
-td_tags = tr_tag.select('td')
+td_tags = tr_tag.select('td') # * 태그 사용 가능
+
 
 for tag in td_tags:
     print(tag.get_text())

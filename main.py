f0aac134ed3b5f5e67deed4483304fa402b181a2
Park-jiyoon <bbpgy1995@gmail.com>
2022-03-26 19:02:17 +0900

    꿈의 직장 전화번호 모으기

--- main.py
+++ main.py
@@ -2,24 +2,31 @@
 import requests
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
+# HTML 코드 받아오기
+response = requests.get("https://workey.codeit.kr/orangebottle/index")
 
-wb = Workbook(write_only=True)
-ws = wb.create_sheet('TV Ratings')
-ws.append(['순위','채널', '프로그램', '시청률'])  #헤더 (row)
-
-response = requests.get("https://workey.codeit.kr/ratings/index")
-
+# BeautifulSoup 사용해서 HTML 코드 정리
 soup = BeautifulSoup(response.text, 'html.parser')
 
-for tr_tag in soup.select('tr')[1:]:
-    td_tags = tr_tag.select('td')
-    row = [
-        td_tags[0].get_text(), #순위
-        td_tags[1].get_text(), #채널
-        td_tags[2].get_text(), #프로그램
-        td_tags[3].get_text(), #시청률
-    ]
+wb = Workbook(write_only="True")
+ws = wb.create_sheet()
 
-    ws.append(row)
+ws.append(['지점 이름', '주소', '전화번호'])
 
-wb.save('시청률_2010년1월1주차.xlsx')
+
+branch_infos = []
+
+# 모든 지점에 대한 태그 가져오기
+branch_tags = soup.select('div.branch')
+
+for branch_tag in branch_tags[1:]:
+    # 각 태그에서 지점 이름, 전화번호 가져오기
+    branch_name = branch_tag.select_one('p.city').get_text()
+    address = branch_tag.select_one('p.address').get_text()
+    phone_number = branch_tag.select_one('span.phoneNum').get_text()
+    ws.append([branch_name, address, phone_number])
+
+
+wb.save('오렌지_보틀.xlsx')
+# 출력 코드
+

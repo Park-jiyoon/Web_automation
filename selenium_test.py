25af01e5b166978359260de6f341c892fcaeb95a
Park-jiyoon <bbpgy1995@gmail.com>
2022-04-02 17:07:04 +0900

    셀리니움 로그인

--- selenium_test.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver = webdriver.Chrome('/Users/nhn/Downloads/chromedriver')
-
-driver.get('https://codeit.kr')

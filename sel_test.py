c72a5834fa81c0885cac047149349eed1d394957
Park-jiyoon <bbpgy1995@gmail.com>
2022-04-03 12:18:59 +0900

    Selenium Explicit Wait 추가

--- sel_test.py
+++ sel_test.py
@@ -2,10 +2,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
 
 driver = webdriver.Chrome('/Users/nhn/Downloads/chromedriver')
 driver.implicitly_wait(3)
